da016aa2f98a9b997cc15bc1cfd1efe8dcd58488
angfigueroa <125718358+angfigueroa@users.noreply.github.com>
2023-08-22 22:37:39 -0600

    Create ADC.h

---
+++ ADC.h
@@ -0,0 +1,8 @@
+#ifndef ADC_H
+#define	ADC_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+void ADC_CONF(int frec);
+void ADC_IF(void);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
